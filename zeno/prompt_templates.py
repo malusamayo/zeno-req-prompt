15d40df898e78079e3c074221fe44a2ac2aeecae
Chenyang Yang <773386@gmail.com>
2024-09-26 11:42:15 -0400

    fix template formating bug

--- zeno/prompt_templates.py
+++ zeno/prompt_templates.py
@@ -141,11 +141,11 @@ Given the user feedback, update the requirements:
 When you update the requirements, you should split requirements into atomic units, e.g., conciseness and format should be separated requirements.
 
 Your response should be in the following JSON format:
-{
+{{
     "actions": [
-        {"action": "update", "requirement_id": "existing_req_id", "updated_description": "new description", "updated_evaluation_method": "evaluation_method", "updated_prompt_snippet": "prompt_snippet"},
-        {"action": "delete", "requirement_id": "existing_req_id"},
-        {"action": "add", "new_requirement": {"name": "new requirement name", "description": "new requirement description", "evaluation_method": "evaluation method of the new requirement which will be executed by GPT", "prompt_snippet" : "prompt implementation of the new requirement"}}
+        {{"action": "update", "requirement_id": "existing_req_id", "updated_description": "new description", "updated_evaluation_method": "evaluation_method", "updated_prompt_snippet": "prompt_snippet"}},
+        {{"action": "delete", "requirement_id": "existing_req_id"}},
+        {{"action": "add", "new_requirement": {{"name": "new requirement name", "description": "new requirement description", "evaluation_method": "evaluation method of the new requirement which will be executed by GPT", "prompt_snippet" : "prompt implementation of the new requirement"}}}}
     ]
-}.  
+}}  
 """
