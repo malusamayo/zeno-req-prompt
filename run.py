5e13614a22f219ae0d5d7f93ba1014a5d3ad8b3a
Chenyang Yang <773386@gmail.com>
2024-11-03 17:25:40 -0500

    add config files

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import yaml
+import argparse
 from zeno.runner import zeno
 from zeno.api import model
 from zeno.api import ZenoParameters, ZenoOptions, ModelReturn
@@ -33,52 +35,27 @@ def openai_inference(model_name, prompt):
     return pred
 
 if __name__ == '__main__':
-    data = pd.read_csv('data/travel_data.csv').sample(10, random_state=42).reset_index(drop=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default='config.yaml')
+    args = parser.parse_args()
+    
+    with open(args.config, 'r') as f:
+        config = yaml.safe_load(f)
+
+    data = pd.read_csv(config["data"]["data_path"]).sample(config["data"]["sample_size"], random_state=42).reset_index(drop=True)
     data["label"] = ""
     prompt = "<prompt></prompt>"
-    
-    # client = OpenAIMultiClient(endpoint="chats", data_template={"model": "gpt-4o-mini"})
-    # def chat_completion(inputs):
-    #     for i, x in enumerate(inputs):
-    #         client.request(
-    #             data={
-    #                 "messages": [
-    #                     {"role": "system", "content": prompt},
-    #                     {"role": "user", "content": x}
-    #                 ],
-    #             }, metadata={'num': i}
-    #         )
-    # client.run_request_function(chat_completion, list(data["question"]), stop_at_end=False)
-    # out = {}
-    # for result in client:
-    #     num = result.metadata['num']
-    #     response = result.response.choices[0].message.content
-    #     out[num] = response
-    # out = [item[1] for item in sorted(out.items())]
-
-    # from tqdm import trange
-    # zeno_options = ZenoOptions(
-    #         id_column="index",
-    #         data_column="question",
-    #         label_column="label",
-    #         distill_columns=dict(),
-    #         data_path="",
-    #         label_path="",
-    #         output_column="",
-    #         output_path="",
-    #     )
-    # out = openai_inference("gpt-4o-mini", prompt)(data, zeno_options)
-    # print(out)
 
     params = ZenoParameters(
         metadata=data,
         functions=[openai_inference],
-        models=['gpt-4o-mini'],
+        models=config["models"],
         prompts={'v1': Prompt(text=prompt, version='v1', requirements={})},
         view='text-classification',
-        data_column="query",
+        data_column=config["data"]["data_column"],
         label_column="label",
-        cache_path='.zeno_cache_102824',
+        cache_path=config["settings"]["cache_path"],
         multiprocessing=False,
+        port=config["settings"]["port"]
     )
     zeno(params)
